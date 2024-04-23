b786c291097d9e76e25c5bb51ff2b0c795c25eae
Anurag Mishra <76533034+anurag8546@users.noreply.github.com>
2024-04-24 03:42:05 +0530

    Updated About.js

--- web_panel/src/pages/About.js
+++ web_panel/src/pages/About.js
@@ -16,17 +16,17 @@ const ProductDetailPage = () => {
 		          <h2>our history</h2>
 		          <span />
 		        </div>
-		        <p className="about-text">
-		          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
-		          accusantium sapiente tempora sed dolore esse deserunt eaque excepturi,
-		          delectus error accusamus vel eligendi, omnis beatae. Quisquam, dicta.
-		          Eos quod quisquam esse recusandae vitae neque dolore, obcaecati incidunt
-		          sequi blanditiis est exercitationem molestiae delectus saepe odio
-		          eligendi modi porro eaque in libero minus unde sapiente consectetur
-		          architecto. Ullam rerum, nemo iste ex, eaque perspiciatis nisi, eum
-		          totam velit saepe sed quos similique amet. Ex, voluptate accusamus
-		          nesciunt totam vitae esse iste.
-		        </p>
+		       <p className="about-text">
+				"Fast fashion" refers to a business model in the clothing industry characterized by rapidly producing and selling inexpensive, trendy clothing items to meet consumer demand for quick, affordable fashion. It often involves resource depletion, exploitative labor practices, and significant environmental impacts. Resulting into producing the textile waste and thus harming the nature.
+
+Alone, India accounts for 8.5% of global textile waste. Fast fashion industries are the worst contributors to our environment. 
+
+Fast fashion’s negative impact on the environment and society can be mitigated through various sustainable practices. Thrifting, the act of buying, renting and selling second-hand or expensive clothing, is a compelling solution that addresses several of the problems associated with fast fashion. Here's how thrifting can help:
+Reduce Textile Waste - Thrifting encourages the reuse and repurposing of clothing items, diverting them from landfills.
+Resource Efficiency - By extending the lifespan of clothing, thrifting reduces the demand for new resource-intensive production.
+Pollution Reduction - Thrifting decreases the need for new textile manufacturing, which is often a source of environmental pollution.
+
+</p>
 		    </section>
 		 	<Sidebar/>
 		 	<Cart/>
